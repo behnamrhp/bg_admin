4fff2e022c9719a0ff81198b88e4b8a7056b1eaa
Behnamrhp <behnamrahimpour74@gmail.com>
2022-01-10 21:49:46 +0330

    first init after slider page

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './assets/css/sidemenu.css';
+import './assets/css/style.css';
+import './assets/css/style1.css';
+import './assets/css/style2.css';
+import './assets/css/style-dark.css';
+import './assets/css/boxed.css';
+import './assets/css/dark-boxed.css';
+import './assets/css/skin-modes.css';
+import './assets/css/animate.css';
 import App from './App';
-import { store } from './app/store';
+import { persistor, store } from './redux/store/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
 
